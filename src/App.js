71ba4228c6eaf97071a73b25d9cf20af02cfd02c
ale <ale@ale.com>
2022-06-23 19:24:43 -0300

    feat: create component HomeInfor and add styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./Components/Header";
+import HomeInfor from "./Components/HomeInfor";
 import Routes from "./Routes/routes";
 
 function App() {
   return (
     <div>
       <Header />
+      <HomeInfor />
       <Routes />
     </div>
   );
