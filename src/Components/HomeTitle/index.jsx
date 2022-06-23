7a03f832440598a0b4c0742648650c8750b2cf30
ale <ale@ale.com>
2022-06-22 21:21:06 -0300

    style: add styled-components to elements of  homeTitle component

--- src/Components/HomeTitle/index.jsx
+++ src/Components/HomeTitle/index.jsx
@@ -1,22 +1,23 @@
 import Button from "../Button";
 
 import { FaUserEdit } from "react-icons/fa";
+import { BoxButton, Container } from "./indexStyle";
 
 const HomeTitle = () => {
   return (
-    <div>
+    <Container>
       <h2>Crie sua gestão de habitos para ajudar na sua rotina </h2>
       <p>
         Planeje os habitos que você deseja obter os organize de forma mais
         adequada para você.
       </p>
-      <div>
+      <BoxButton>
         <Button>
-          Cadastre-se <FaUserEdit size="10" color="var(--color-second-light)" />
+          Cadastre-se <FaUserEdit size="15" color="var(--color-second-light)" />
         </Button>
         <span>Plataforma gratuita? faça seu cadastro</span>
-      </div>
-    </div>
+      </BoxButton>
+    </Container>
   );
 };
 
