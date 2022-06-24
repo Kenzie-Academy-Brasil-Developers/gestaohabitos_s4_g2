b662984718f818f7004e32a90a640f9e55625d06
Olgaluiisa <olgaluiisa@hotmail.com>
2022-06-24 17:37:20 -0300

    style: finished component styling

--- src/Components/CardGroup/index.jsx
+++ src/Components/CardGroup/index.jsx
@@ -1,24 +1,31 @@
+import {
+  CardContent,
+  Container,
+  GroupMembersContainer,
+  LineBox,
+} from "./indexStyle";
+
 const CardGroup = () => {
   return (
-    <section>
-      <div></div>
-      <div>
+    <Container>
+      <LineBox></LineBox>
+      <CardContent>
         <h3>Aprendendo React juntos</h3>
         <h5>descrição:</h5>
         <p>
-          Somos um grupo de pessoas que possuem <strong>tdah</strong> e queremos
-          nos unir para estudarmos todos os dias reactjs{" "}
+          Somos um grupo de pessoas que possuem <span>tdah</span> e queremos nos
+          unir para estudarmos todos os dias reactjs{" "}
         </p>
-        <div>
+        <GroupMembersContainer>
           <h5>Membros</h5>
           <div>
             <div></div>
             <div></div>
             <div></div>
           </div>
-        </div>
-      </div>
-    </section>
+        </GroupMembersContainer>
+      </CardContent>
+    </Container>
   );
 };
 export default CardGroup;
---
+++ src/Components/CardGroup/indexStyle.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  width: 70%;
+  max-width: 308px;
+  margin: 10px auto;
+  padding: 25px;
+  display: flex;
+  background-color: var(--color-primary-light);
+  box-shadow: 4px 4px 4px 0 rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+`;
+
+export const LineBox = styled.div`
+  width: 9px;
+  height: 179px;
+  margin-right: 10px;
+  background-color: var(--color-tag-level-easy);
+`;
+
+export const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  color: var(--color-text-light);
+
+  h3 {
+    margin-bottom: 20px;
+    font-size: 1.1rem;
+  }
+  h5 {
+    margin-bottom: 8px;
+    font-size: 0.95rem;
+  }
+  p {
+    font-size: 0.85rem;
+    font-weight: lighter;
+    margin-bottom: 10px;
+    span {
+      color: var(--color-tag-level-easy);
+    }
+  }
+`;
+export const GroupMembersContainer = styled.div`
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: start;
+
+    div {
+      width: 31px;
+      height: 31px;
+      background-color: #d9d9d9;
+      margin-right: 5px;
+      border-radius: 100%;
+    }
+  }
+`;
